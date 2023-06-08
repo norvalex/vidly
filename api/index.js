c35ee0c89cc94d4d570844c821821cb51cde2775
Alex <norvalex@users.noreply.github.com>
2023-06-08 12:29:29 +0200

    .

--- api/index.js
+++ api/index.js
@@ -2,12 +2,12 @@ const express = require("express");
 const winston = require("winston");
 const app = express();
 
-require("./startup/logging")();
-require("./startup/routes")(app);
-require("./startup/db")();
-require("./startup/config")();
-require("./startup/validation")();
-require("./startup/prod")(app);
+require("../startup/logging")();
+require(".,/startup/routes")(app);
+require(",./startup/db")();
+require(".,/startup/config")();
+require(",./startup/validation")();
+require("../startup/prod")(app);
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () =>
