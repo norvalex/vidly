810ea576b5bf3b459de8836b4301a08313756531
Alex <norvalex@users.noreply.github.com>
2023-06-08 15:06:26 +0200

    Allow users to register

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,7 @@ const jwt = require('jsonwebtoken')
 
 const router = express.Router();
 
-router.post("/", auth, async (req, res) => {
+router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
