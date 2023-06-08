61f13c82968ab737ffc2a5736b19d9e4422ab6a2
Alex <norvalex@users.noreply.github.com>
2023-06-08 12:56:31 +0200

    move app to api/ folder

--- api/index.js
+++ api/index.js
@@ -2,16 +2,16 @@ const express = require("express");
 const winston = require("winston");
 const app = express();
 
-require("../startup/logging")();
-require(".,/startup/routes")(app);
-require(",./startup/db")();
-require(".,/startup/config")();
-require(",./startup/validation")();
-require("../startup/prod")(app);
+require("./startup/logging")();
+require("./startup/routes")(app);
+require("./startup/db")();
+require("./startup/config")();
+require("./startup/validation")();
+require("./startup/prod")(app);
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () =>
   winston.info(`Listening on port ${port}...`)
 );
-// some textcc
+// some text
 module.exports = server;
